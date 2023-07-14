14660861dc0cd2c661413ee617c5bc2974fb4d89
rafipopz98 <itsrealrafi@gmail.com>
2023-07-14 19:08:55 +0530

    database connected

--- server/app.js
+++ server/app.js
@@ -1,22 +1,26 @@
 import express from 'express'
-
+import mongoose from 'mongoose'
 const app=express();
 
+// DATABASE
 
-
+const DB='mongodb+srv://rafi:rafi@cluster0.yfjisy3.mongodb.net/?retryWrites=true&w=majority'
+mongoose.connect(DB).then(()=>{
+    console.log("connection successfull")
+}).catch((err)=>console.log('the error is ',err))
 // MIddleware
 
 
-const middleware=(req,res,next)=>{
-    console.log("middlwware")
-    next()
-}
+// const middleware=(req,res,next)=>{
+//     console.log("middlwware")
+//     next()
+// }
 
 
 app.get('/',(req,res)=>{
     res.send("hello world")
 });
-app.get('/about',middleware,(req,res)=>{
+app.get('/about',(req,res)=>{
     res.send("hello world from about")
 });
 app.get('/contact',(req,res)=>{
