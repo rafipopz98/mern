30805b99cee2ce15936f49f4041e08d458b7dbd3
rafipopz98 <itsrealrafi@gmail.com>
2023-07-14 19:38:12 +0530

    dot env and files

--- server/app.js
+++ server/app.js
@@ -1,39 +1,39 @@
-import express from 'express'
-import mongoose from 'mongoose'
-const app=express();
+const express = require("express");
+const dotenv = require("dotenv");
 
+const app = express();
 // DATABASE
+dotenv.config({
+  path: './config.env',
+});
+require("./db/conection");
 
-const DB='mongodb+srv://rafi:rafi@cluster0.yfjisy3.mongodb.net/?retryWrites=true&w=majority'
-mongoose.connect(DB).then(()=>{
-    console.log("connection successfull")
-}).catch((err)=>console.log('the error is ',err))
+
+const PORT = process.env.PORT;
+
 // MIddleware
-
 
 // const middleware=(req,res,next)=>{
 //     console.log("middlwware")
 //     next()
 // }
 
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+app.get("/about", (req, res) => {
+  res.send("hello world from about");
+});
+app.get("/contact", (req, res) => {
+  res.send("hello world from contact");
+});
+app.get("/signin", (req, res) => {
+  res.send("hello world from sign in");
+});
+app.get("/signup", (req, res) => {
+  res.send("hello world from signup");
+});
 
-app.get('/',(req,res)=>{
-    res.send("hello world")
+app.listen(PORT, () => {
+  console.log(`server is runningon port ${PORT}`);
 });
-app.get('/about',(req,res)=>{
-    res.send("hello world from about")
-});
-app.get('/contact',(req,res)=>{
-    res.send("hello world from contact")
-});
-app.get('/signin',(req,res)=>{
-    res.send("hello world from sign in")
-});
-app.get('/signup',(req,res)=>{
-    res.send("hello world from signup")
-});
-
-
- app.listen(3000,()=>{
-    console.log("server is running")
- })
