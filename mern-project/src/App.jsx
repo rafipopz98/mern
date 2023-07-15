f3ef4b4dd6d6f7c97c13173ef20373e4443aa926
rafipopz98 <itsrealrafi@gmail.com>
2023-07-15 16:39:52 +0530

    navbar

---
+++ mern-project/src/App.jsx
@@ -0,0 +1,21 @@
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+function App() {
+  return <>
+<Router>
+  <Navbar />
+  <Routes>
+    <Route exact path={'/'} element={<Home/>}/>
+    <Route exact path={'/contact'} element={<Contact/>}/>
+    <Route exact path={'/about'} element={<About/>}/>
+    <Route exact path={'*'} element={<Home/>}/>
+
+  </Routes>
+</Router>
+  </>;
+}
+
+export default App;
