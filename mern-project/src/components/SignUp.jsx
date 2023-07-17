e3fa1020c78f2988da1af6a99ed48b73d64160cd
rafipopz98 <itsrealrafi@gmail.com>
2023-07-17 23:12:38 +0530

    signup

--- mern-project/src/components/SignUp.jsx
+++ mern-project/src/components/SignUp.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const SignUp = () => {
   return (
-    <div>SignUp</div>
+    <div>
+      name <input type="text" />
+      email<input type="password" />
+      password<input type="password" />
+      cpassword<input type="password" />
+      phone<input type="number" />
+
+    </div>
   )
 }
 
