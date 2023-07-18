bce48ab63a684b35a0e113d4bf100ee3df97cc2c
rafipopz98 <itsrealrafi@gmail.com>
2023-07-18 23:04:56 +0530

    signup

--- mern-project/src/components/SignUp.jsx
+++ mern-project/src/components/SignUp.jsx
@@ -4,16 +4,15 @@ const SignUp = () => {
   return (
     <div>
     <form >
-
       name <input type="text" />
       email<input type="password" />
       password<input type="password" />
       cpassword<input type="password" />
       phone<input type="number" />
+     
       <input type="submit" value="signup" />
     </form>
     </div>
   )
 }
-
 export default SignUp
